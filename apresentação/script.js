b6d0786b35b1889aa2eb970890664032cceda46e
Bruno Queiroz <82784350+buqueiroz@users.noreply.github.com>
2022-01-12 22:45:33 -0300

    Add files via upload

---
+++ apresentação/script.js
@@ -0,0 +1,17 @@
+const cafe = document.querySelector(".cafe");
+
+let cor;
+function digitacao(texto, contador) {
+  if (contador < texto.length) {
+    setTimeout(() => {
+      cafe.textContent += texto.charAt(contador);
+      contador++;
+      digitacao(texto, contador);
+    }, 89);
+  }
+}
+
+digitacao("MUITO CAFÉ E NOITES ACORDADO.", 0);
+
+
+
